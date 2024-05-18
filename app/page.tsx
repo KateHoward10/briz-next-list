18a408ce19705dabbe7c6e07ca6a10cae1317017
Kate Howard <k.l.howard.1992@gmail.com>
2024-05-18 18:08:43 +0100

    Set up Next project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="mx-auto lg:mx-32 xl:max-w-screen-lg xl:mx-auto my-12 px-5">
+      <button className="border border-2 dark:border-gray-700 cursor-pointer rounded px-2 font-semibold hover:bg-gray-200 dark:hover:bg-gray-700">
+        Log in
+      </button>
+    </main>
+  );
+}
